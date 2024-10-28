2a40b8a3ac6f537701ffa85259e25701d5dc15f5
rsolano <raul.solano@movicoders.com>
2024-10-28 15:25:32 +0100

    First commit

---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,55 @@
+import { useMemo } from "react";
+import { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
+
+type OrderTotalsProps = {
+  order: OrderItem[];
+  tip: number;
+  placeOrder: () => void;
+};
+
+export default function OrderTotals({
+  order,
+  tip,
+  placeOrder,
+}: Readonly<OrderTotalsProps>) {
+  const subTotalAmout = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
+  const tipAmount = useMemo(() => tip * subTotalAmout, [tip, subTotalAmout]);
+
+  const totalAmount = useMemo(
+    () => tipAmount + subTotalAmout,
+    [subTotalAmout, tipAmount]
+  );
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl">Totales y Propina: </h2>
+      </div>
+      <p>
+        Subtotal a pagar: {""}
+        <span className="font-bold">{formatCurrency(subTotalAmout)}</span>
+      </p>
+      <p>
+        Propina: {""}
+        <span className="font-bold">{formatCurrency(tipAmount)}</span>
+      </p>
+      <p>
+        Total a pagar: {""}
+        <span className="font-bold">{formatCurrency(totalAmount)}</span>
+      </p>
+
+      <button
+        className="w-full bg-black p-3 uppercase text-white mt-10 font-bold disabled:opacity-10"
+        disabled={totalAmount === 0}
+        onClick={placeOrder}
+      >
+        Guardar orden
+      </button>
+    </>
+  );
+}
